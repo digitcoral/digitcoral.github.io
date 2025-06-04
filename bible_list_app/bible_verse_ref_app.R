1d93d692b94cc4bd23ff0c12645533142ea090d1
digitcoral <digitcoral@gmail.com>
2025-06-03 20:04:53 -0500

    update and close

--- bible_list_app/bible_verse_ref_app.R
+++ bible_list_app/bible_verse_ref_app.R
@@ -1,14 +1,24 @@
 ### Testing codes to the idea of when click a data row, then show some detail contents.
 
+library(googlesheets4)
+library(dplyr)
 library(shiny)
-# library(dplyr)
 library(data.table)
 
-DT <- copy(mtcars)
+gs4_deauth() #<--use only for googlesheets shared as 'anyone with the link'
+
+bok_indx <- read_sheet("https://docs.google.com/spreadsheets/d/1RDLRFS7o2lSLsIRJAjd-cVXQDt_Q1XiDxIQ_ZC6_VDs/edit?usp=sharing", sheet ="Index")
+bk42     <- read_sheet("https://docs.google.com/spreadsheets/d/1RDLRFS7o2lSLsIRJAjd-cVXQDt_Q1XiDxIQ_ZC6_VDs/edit?usp=sharing", sheet ="42-路-路加福音")
+  
+tot_chaps <- prettyNum(sum(bok_indx$Total_Chapters),big.mark=',',scientific=F)
+tot_vers <- prettyNum(sum(bok_indx$Total_Verses),big.mark=',',scientific=F)
+titl <- quote(as.character(bok_indx[42,"Book"]))
+
+DT <- copy(bk42)
 setDT(DT)
 
 ui <- fluidPage(
-  titlePanel("mtcars"),
+  titlePanel(".(titl)"),
   mainPanel(
     tableOutput("table")
   )
